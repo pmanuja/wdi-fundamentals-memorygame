9813b5179b318d91124319616c29fbf2123bbc29
pooja <manuja.pooja@gmail.com>
2019-03-03 16:04:28 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var queen = "queen";
+var queentMatch = "queen";
+var king = "king";
+var kingMatch = "king";
+console.log("User flipped " + queen);
+console.log("User flipped " + king);
